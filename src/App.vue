6be2d6baf1c99af6086b4c049977cd31dd0ae3d8
luthfiyyah hanifah amari <luthfiyyah.205025@gmail.com>
2022-10-30 20:07:24 +0700

    initial commit, membuat template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <navbar/>
   <router-view/>
 </template>
+
+<script>
+  import Navbar from '@/components/Navbar.vue'
+
+  export default {
+    name: 'AppView',
+    components: {
+      Navbar
+    }
+  }
+</script>
 
 <style>
 #app {
